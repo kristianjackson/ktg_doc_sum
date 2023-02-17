7c32f34364042009cebc72cf0c9602f8d6741ffa
kristianjackson <kristian.jackson@gmail.com>
2023-02-17 12:23:44 -0500

    removed the dotenv references

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from dotenv import load_dotenv
 import uuid
 
 import openai
